347f6f38e6e1a8d71af7dc207aa105018cd1f288
gaevivan <gaev93@ya.ru>
2020-12-25 14:58:33 +0300

    shit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,12 @@
 import { NgModule, Type } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-// import { NotFoundPageModule } from './pages/not-found-page/not-found-page.module';
-// import { NotFoundPageModule } from './pages/not-found-page/not-found-page.module';
-import { PagesModule } from './pages/pages.module';
+import { PagesModule } from './features/pages/pages.module';
 
 const routes: Routes = [
     {
         path: '**',
         loadChildren: (): Promise<Type<PagesModule>> =>
-            import('./pages/pages.module').then((module) => module.PagesModule)
+            import('./features/pages/pages.module').then((module) => module.PagesModule)
     }
 ];
 
